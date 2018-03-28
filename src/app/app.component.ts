f716f572117ae6ead631f320b11f943decb9c1b9
jyothigundra <37636264+jyothigundra@users.noreply.github.com>
2018-03-28 10:24:38 +0200

    ngFor and trackby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [
+  courses;
+
+ loadCourses(){
+  this.courses=[
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
     { id: 3, name: 'course3' }
   ];
-onAdd(){
-  this.courses.push({id: 4, name: 'course4'});
 }
-onRemove(course)
-{
-  let index= this.courses.indexOf(course);
-  this.courses.splice(index, 1);
-
+trackCourses(index,course){
+ return course ? course.id : undefined ;
 }
 }
