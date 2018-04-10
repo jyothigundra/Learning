09f5093a02c5e49fe83e69a5c5a9297355acf5af
jyothigundra <37636264+jyothigundra@users.noreply.github.com>
2018-04-10 10:42:44 +0200

    update and create posts

--- src/app/components/posts-component/posts-component.component.ts
+++ src/app/components/posts-component/posts-component.component.ts
@@ -8,11 +8,25 @@ import { HttpClient } from '@angular/common/Http';
 })
 export class PostsComponentComponent {
   posts: any;
+  private url='https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient) { 
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(response =>{
+  constructor(private http: HttpClient) { 
+    http.get(this.url).subscribe(response =>{
       this.posts = response;
     });
   } 
+  createPosts(input :HTMLInputElement){
+    let request= {title: input.value };
+    input.value='';
+      this.http.post(this.url, JSON.stringify(request)).subscribe(response =>{
+        request['id']= response['id'];
+        this.posts.splice(0,0,request);
+        console.log(response);
+      })
+  }
+  updatePost(post){
+    this.http.patch(this.url+'/'+post.id,JSON.stringify({isread:true})).subscribe(response=>
+    console.log(response));
+  }
   
 }
